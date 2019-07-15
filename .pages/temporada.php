2df85ed7cc0748a35e14d62164c8fd26a94134ff
lucasviniciussilva <52464978+lucasviniciussilva@users.noreply.github.com>
2019-07-15 12:59:52 -0300

    alterações

--- .pages/temporada.php
+++ .pages/temporada.php
@@ -1,10 +1,10 @@
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
 <head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Add icon library -->
@@ -16,165 +16,166 @@
 
     <style>
     </style>
-    
+
 </head>
 <body style="background-image: url('../imagens/.jpg'); background-size:cover;">
-    
-    
-     <section class="" style="background-image: url('../imagens/background-gundam.png'); background-size:cover;">
-        
-        <div class="" style="">
-            <h2 style="text-align:center;">Temporada</h2>
-        </div>
-        
-        <!-- Cards: Winter, Spring, Summer, Fall -->
-        <div class="row">
-            
-            <div class="col">
-                <a target="_blank" href="https://www.behance.net/doonnnx">
-                  <div class="thecard">
+
+<section class="" style="background-image: url('../imagens/background-gundam.png'); background-size:cover;">
+
+    <?php include ('../.globals/header.php'); ?>
+
+    <div class="" style="">
+        <h2 style="text-align:center;">Temporada</h2>
+    </div>
+
+    <!-- Cards: Winter, Spring, Summer, Fall -->
+    <div class="row">
+
+        <div class="col">
+            <a target="_blank" href="https://www.behance.net/doonnnx">
+                <div class="thecard">
 
                     <div class="card-img">
-                      <img src="../imagens/seaseon/winter-anime.png">
+                        <img src="../imagens/seaseon/winter-anime.png">
                     </div>
 
                     <div class="card-caption">
-                      <i class="far fa-snowflake" style="font-size:20px; color:#5500ff;"></i>
-                      <span class="date">Janeiro, Julho 16, 2019</span>
-                      <h1>Janeiro / Winter / Inverno</h1>
-                      <p>Um guia com os novos episódio da temporada de Janeiro / Winter.</p>
+                        <i class="far fa-snowflake" style="font-size:30px; color:#5500ff;"></i>
+                        <span class="date">Janeiro, Julho 16, 2019</span>
+                        <h1>Janeiro/Winter/Inverno</h1>
+                        <p>Um guia com os novos episódio da temporada de Janeiro / Winter.</p>
                     </div>
 
                     <div class="card-outmore">
-                      <h5>Vê Animes</h5>
-                      <i id="outmore-icon" class="fa fa-angle-right"></i>
+                        <h5>Vê Animes</h5>
+                        <i id="outmore-icon" class="fa fa-angle-right"></i>
                     </div>
 
-                  </div>
-                </a>
-            </div>
-            
-            <div class="col">
-                <a target="_blank" href="https://www.behance.net/doonnnx">
-                  <div class="thecard">
+                </div>
+            </a>
+        </div>
+
+        <div class="col">
+            <a target="_blank" href="https://www.behance.net/doonnnx">
+                <div class="thecard">
 
                     <div class="card-img">
-                      <img src="../imagens/seaseon/spring-anime.jpg">
+                        <img src="../imagens/seaseon/spring-anime.jpg">
                     </div>
 
                     <div class="card-caption">
-                      <i class="fab fa-pagelines" style="font-size:23px; color:pink;"></i>
-                      <span class="date">Thursday, July 16, 2015</span>
-                      <h1>Abril / Spring / Primavera</h1>
-                      <p>Um guia com os novos episódio da temporada de Abril / Primavera.</p>
+                        <i class="fab fa-pagelines" style="font-size:23px; color:pink;"></i>
+                        <span class="date">Thursday, July 16, 2015</span>
+                        <h1>Abril/Spring/Primavera</h1>
+                        <p>Um guia com os novos episódio da temporada de Abril / Primavera.</p>
                     </div>
 
                     <div class="card-outmore">
-                      <h5>Vê Animes</h5>
-                      <i id="outmore-icon" class="fa fa-angle-right"></i>
+                        <h5>Vê Animes</h5>
+                        <i id="outmore-icon" class="fa fa-angle-right"></i>
                     </div>
 
-                  </div>
-                </a>
-            </div>
-            
-            <div class="col">
-                <a target="_blank" href="https://www.behance.net/doonnnx">
-                  <div class="thecard">
+                </div>
+            </a>
+        </div>
+
+        <div class="col">
+            <a target="_blank" href="https://www.behance.net/doonnnx">
+                <div class="thecard">
 
                     <div class="card-img">
-                      <img src="../imagens/seaseon/summer-%20anime.jpg">
+                        <img src="../imagens/seaseon/summer-%20anime.jpg">
                     </div>
 
                     <div class="card-caption">
-                      <i class="fas fa-umbrella-beach" style="font-size:25px; color:#0099ff;"></i>
-                      <span class="date">Thursday, July 16, 2015</span>
-                      <h1>Julho / Summer / Verão</h1>
-                      <p>Um guia com os novos episódio da temporada de Julho / Verão.</p>
+                        <i class="fas fa-umbrella-beach" style="font-size:25px; color:#0099ff;"></i>
+                        <span class="date">Thursday, July 16, 2015</span>
+                        <h1>Julho/Summer/Verão</h1>
+                        <p>Um guia com os novos episódio da temporada de Julho / Verão.</p>
                     </div>
 
                     <div class="card-outmore">
-                      <h5>Vê Animes</h5>
-                      <i id="outmore-icon" class="fa fa-angle-right"></i>
+                        <h5>Vê Animes</h5>
+                        <i id="outmore-icon" class="fa fa-angle-right"></i>
                     </div>
 
-                  </div>
-                </a>
-            </div>
-            
-            <div class="col">
-                <a target="_blank" href="https://www.behance.net/doonnnx">
-                  <div class="thecard">
+                </div>
+            </a>
+        </div>
+
+        <div class="col">
+            <a target="_blank" href="https://www.behance.net/doonnnx">
+                <div class="thecard">
 
                     <div class="card-img">
-                      <img src="../imagens/seaseon/fall-anime.jpg">
+                        <img src="../imagens/seaseon/fall-anime.jpg">
                     </div>
 
                     <div class="card-caption">
-                      <i class="fas fa-apple-alt" style="font-size:25px; color:#e62e00;"></i>
-                      <span class="date">Thursday, July 16, 2015</span>
-                      <h1>Outubro / Fall / Outono</h1>
-                      <p>Um guia com os novos episódio da temporada de Outubro / Fall.</p>
+                        <i class="fas fa-apple-alt" style="font-size:25px; color:#e62e00;"></i>
+                        <span class="date">Thursday, July 16, 2015</span>
+                        <h1>Outubro/Fall/Outono</h1>
+                        <p>Um guia com os novos episódio da temporada de Outubro / Fall.</p>
                     </div>
 
                     <div class="card-outmore">
-                      <h5>Vê Animes</h5>
-                      <i id="outmore-icon" class="fa fa-angle-right"></i>
+                        <h5>Vê Animes</h5>
+                        <i id="outmore-icon" class="fa fa-angle-right"></i>
                     </div>
 
-                  </div>
-                </a>
-            </div>
-            
+                </div>
+            </a>
         </div>
-        
-        <!-- Container: Animes de Temporadas passadas -->
-        <div class="container">
-            <div class="" style="background-color:#efefef; width:1140px; margin-left:-15px; border-radius:2px; height:40px;">
-                <h3 style="text-align:center;">Lista de Animes das Temporada passada</h3>
+
+    </div>
+
+    <!-- Container: Animes de Temporadas passadas -->
+    <div class="container" style="margin-top: 100px;">
+        <div class="" style="background-color:#efefef; width:1140px; margin-left:-15px; border-radius:2px; height:40px;">
+            <h3 style="text-align:center;">Lista de Animes das Temporada passada</h3>
+        </div>
+        <div class="row" style="color:black; background-color:white; margin-top:-px;border-radius:2px;">
+            <div class="col">
+                <p> <i class="far fa-snowflake" style="font-size:30px; color:#5500ff;"></i> Janeiro / Winter / Inverno 2018 </p>
+                <p> <i class="far fa-snowflake" style="font-size:30px; color:#5500ff;"></i> Janeiro / Winter / Inverno 2017 </p>
+                <p> <i class="far fa-snowflake" style="font-size:30px; color:#5500ff;"></i> Janeiro / Winter / Inverno 2016 </p>
+                <p> <i class="far fa-snowflake" style="font-size:30px; color:#5500ff;"></i> Janeiro / Winter / Inverno 2018 </p>
             </div>
-            <div class="row" style="color:black; background-color:white; margin-top:-px;border-radius:2px;">
-                <div class="col">
-                    <p> <i class="fab fa-ethereum"></i>Janeiro / Winter / Inverno 2018 </p>
-                    <p> <i class="fab fa-ethereum"></i>Janeiro / Winter / Inverno 2017 </p>
-                    <p> <i class="fab fa-ethereum"></i>Janeiro / Winter / Inverno 2016 </p>
-                    <p> <i class="fab fa-ethereum"></i>Janeiro / Winter / Inverno 2018 </p>
-                </div>
-                <div class="col">
-                    <p> <i class="fab fa-ethereum"></i>Abril / Spring / Primavera 2018 </p>
-                    <p> <i class="fab fa-ethereum"></i>Abril / Spring / Primavera 2017 </p>
-                    <p> <i class="fab fa-ethereum"></i>Abril / Spring / Primavera 2016 </p>
-                    <p> <i class="fab fa-ethereum"></i>Abril / Spring / Primavera 2015 </p>
-                </div>
-                <div class="col">
-                    <p> <i class="fab fa-ethereum"></i>Julho / Summer / Verão 2018 </p>
-                    <p> <i class="fab fa-ethereum"></i>Julho / Summer / Verão 2017 </p>
-                    <p> <i class="fab fa-ethereum"></i>Julho / Summer / Verão 2016 </p>
-                    <p> <i class="fab fa-ethereum"></i>Julho / Summer / Verão 2015 </p>
-                </div>
-                <div class="col">
-                    <p> <i class="fab fa-ethereum"></i>Outubro / Fall / Outono 2018 </p>
-                    <p> <i class="fab fa-ethereum"></i>Outubro / Fall / Outono 2017 </p>
-                    <p> <i class="fab fa-ethereum"></i>Outubro / Fall / Outono 2016 </p>
-                    <p> <i class="fab fa-ethereum"></i>Outubro / Fall / Outono 2015 </p>
-                </div>
+            <div class="col">
+                <p> <i class="fab fa-pagelines" style="font-size:30px; color:pink;"></i> Abril / Spring / Primavera 2018 </p>
+                <p> <i class="fab fa-pagelines" style="font-size:30px; color:pink;"></i> Abril / Spring / Primavera 2017 </p>
+                <p> <i class="fab fa-pagelines" style="font-size:30px; color:pink;"></i> Abril / Spring / Primavera 2016 </p>
+                <p> <i class="fab fa-pagelines" style="font-size:30px; color:pink;"></i> Abril / Spring / Primavera 2015 </p>
+            </div>
+            <div class="col">
+                <p> <i class="fas fa-umbrella-beach" style="font-size:30px; color:#0099ff;"></i> Julho / Summer / Verão 20 </p>
+                <p> <i class="fas fa-umbrella-beach" style="font-size:30px; color:#0099ff;"></i> Julho / Summer / Verão 2017 </p>
+                <p> <i class="fas fa-umbrella-beach" style="font-size:30px; color:#0099ff;"></i> Julho / Summer / Verão 2016 </p>
+                <p> <i class="fas fa-umbrella-beach" style="font-size:30px; color:#0099ff;"></i> Julho / Summer / Verão 2015 </p>
+            </div>
+            <div class="col">
+                <p> <i class="fas fa-apple-alt" style="font-size:30px; color:#e62e00;"></i> Outubro / Fall / Outono 2018 </p>
+                <p> <i class="fas fa-apple-alt" style="font-size:30px; color:#e62e00;"></i> Outubro / Fall / Outono 2017 </p>
+                <p> <i class="fas fa-apple-alt" style="font-size:30px; color:#e62e00;"></i> Outubro / Fall / Outono 2016 </p>
+                <p> <i class="fas fa-apple-alt" style="font-size:30px; color:#e62e00;"></i> Outubro / Fall / Outono 2015 </p>
             </div>
         </div>
-        
-        <!-- Next Section: Button -->
-        <div class="">
-            <button id="button-next" style="margin-top:150px;"> <i class="fa fa-angle-double-down" id="arrow-next"></i> </button>
-        </div>
-        
-    </section>
-    
-    
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
+    </div>
+
+    <!-- Next Section: Button -->
+    <div class="">
+        <button id="button-next" style="margin-top:150px;"> <i class="fa fa-angle-double-down" id="arrow-next"></i> </button>
+    </div>
+
+</section>
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
