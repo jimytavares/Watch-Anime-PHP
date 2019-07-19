7998440f07de60676dfac36cf43469c862568e7a
Yuri Sanchez <sanchezsobral@gmail.com>
2019-07-19 12:55:47 -0300

    minha tela cagada!

--- .pages/lancamentos.php
+++ .pages/lancamentos.php
@@ -16,19 +16,80 @@
 
     <style>
 
+        .bodyanimeted{
+            background: linear-gradient( to left, #2438FF, #8E30FF, #B120E8, #FE24FF);
+            background-size: 400% 400%;
+            position: relative;
+            animation: change 10s ease-in-out infinite;
+        }
+
+        *{
+            box-sizing: border-box;
+        }
+
+
+        @keyframes change {
+            0%{background-position: 0 50%}
+            50%{background-position: 100% 50%}
+            100%{background-position: 0 50%}
+        }
+        .card-list{
+            margin-top: 25px;
+            display: inline-grid;
+            grid-template-columns: repeat(3,minmax(390px,460px));
+            grid-column-gap: 50px;
+            grid-row-gap: 50px;
+
+        }
+        .cover{
+            cursor: pointer;
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+            position: relative;
+            z-index: 3;
+        }
+        .container{margin-left: 5em;}
+        .card{
+            width: 380px;
+            height: 280px;
+            min-width: 370px;
+            background-color: #e9e9e9;
+            display: inline-grid;
+            grid-template-columns: 185px auto;
+        }
+        .card img{
+            width: 100%;
+            height: 100%;
+            position: relative;
+        }
+        .card a{
+            text-decoration: unset;
+            text-align: center;
+        }
+        .overlay{
+            content: "";
+            position: absolute;
+            flex-direction: column;
+            justify-content: flex-end;
+            z-index: 10;
+            width: 100%;
+        }
     </style>
     
 </head>
-<body style="background-image: url('../imagens/img245.jpg'); background-size:cover;">
+<body class="bodyanimeted">
     
     <section>
-       <?php include ('../.globals/header.php'); ?>
-        
-        
-        
-        
-        
-        
+        <div class="fixed-headerbar">
+            <?php include ('../.globals/header.php'); ?>
+        </div>
+        <?php
+            include_once('../control/selectAnimes.php');
+        ?>
+        <div class="fixed-bottom">
+            <?php include ('../.globals/footer.php'); ?>
+        </div>
     </section>
     
     
